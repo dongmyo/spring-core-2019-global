4adcc30dc4a1785a78be5b9457e2fea68064fa14
신동민/NE <dongmyo@nhnent.com>
2019-01-28 17:16:54 +0900

    * @ComponentScan using marker interface

--- src/main/java/com/nhnent/edu/spring_core/config/Config.java
+++ src/main/java/com/nhnent/edu/spring_core/config/Config.java
@@ -1,10 +1,11 @@
 package com.nhnent.edu.spring_core.config;
 
+import com.nhnent.edu.spring_core.service.PackageMarker;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.nhnent.edu.spring_core")
+@ComponentScan(basePackageClasses = { PackageMarker.class })
 public class Config {
 
 }
---
+++ src/main/java/com/nhnent/edu/spring_core/service/PackageMarker.java
@@ -0,0 +1,4 @@
+package com.nhnent.edu.spring_core.service;
+
+public interface PackageMarker {
+}
