6addf180c0d52b6fd9a5933e0e25f3dd14c3ab8a
신동민/NE <dongmyo@nhnent.com>
2019-01-29 17:45:36 +0900

    * @Value("${...}") + PropertySourcesPlaceholderConfigurer (without @PropertySource)

--- src/main/java/com/nhnent/edu/spring_core/config/DatabaseConfig.java
+++ src/main/java/com/nhnent/edu/spring_core/config/DatabaseConfig.java
@@ -1,31 +1,42 @@
 package com.nhnent.edu.spring_core.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+// TODO : #2 instead of @PropertySource.
+//@PropertySource("classpath:datasource.properties")
 public class DatabaseConfig {
-    @Autowired
-    private Environment env;
+    // TODO : #1 use @Value("${...}")
+    @Value("${datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${datasource.url}")
+    private String url;
+
+    @Value("${datasource.username:sa}")
+    private String username;
+
+    @Value("${datasource.password:}")
+    private String password;
 
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
 
-        dataSource.setDriverClassName(env.getProperty("datasource.driver-class-name"));
-        dataSource.setUrl(env.getProperty("datasource.url"));
-        dataSource.setUsername(env.getProperty("datasource.username"));
-        dataSource.setPassword(env.getProperty("datasource.password", ""));
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         dataSource.setInitialSize(10);
         dataSource.setMaxTotal(10);
@@ -64,6 +75,15 @@ public class DatabaseConfig {
         jdbcTemplate.setResultsMapCaseInsensitive(true);
 
         return jdbcTemplate;
+    }
+
+    // TODO : #3 use PropertySourcesPlaceholderConfigurer.
+    // does it work?
+    @Bean
+    public /*static*/ PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        placeholderConfigurer.setLocation(new ClassPathResource("datasource.properties"));
+        return placeholderConfigurer;
     }
 
 }
