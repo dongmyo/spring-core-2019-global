24be6dbcbac00324ce72cf9057a387b9e4efd1a8
신동민/NE <dongmyo@nhnent.com>
2019-02-14 00:22:54 -0800

    * TODO 메세지 수정

--- src/main/java/com/nhnent/edu/spring_core/aop/MeasuringAspect.java
+++ src/main/java/com/nhnent/edu/spring_core/aop/MeasuringAspect.java
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 // TODO : #1 `com.nhnent.edu.spring_core.repository.NotiLogDao.insertLog` 메쏘드 수행시간 로깅을 위한 aspect.
+// TODO : #1 aspect for logging the execution time of `com.nhnent.edu.spring_core.repository.NotiLogDao.insertLog` method. 
 @Component
 @Aspect
 // TODO : #2 aspect간 우선순위 지정.
+// TODO : #2 set order among aspects.
 @Order(1)
 public class MeasuringAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(MeasuringAspect.class);
@@ -35,6 +37,11 @@ public class MeasuringAspect {
          * TODO : #3 만약 ...
          * joinPoint.process()를 하지 않으면?
          * 다른 값을 return 하면?
+         */
+        /*
+         * TODO : #3 what if ...
+         * joinPoint.proceed() is removed ?
+         * wrong value is returned
          */
         return result;
     }
